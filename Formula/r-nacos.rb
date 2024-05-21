f7bc99ad167bdcb45f955cace7fb50a62729fcd7
heqingpan <heqingpan@126.com>
2024-05-22 01:07:52 +0800

    update version to v0.5.9

--- Formula/r-nacos.rb
+++ Formula/r-nacos.rb
@@ -4,12 +4,28 @@
 class RNacos < Formula
   desc "r-nacos"
   homepage "https://github.com/r-nacos/r-nacos"
-  url "https://github.com/nacos-group/r-nacos/releases/download/v0.5.7/rnacos-x86_64-apple-darwin.tar.gz"
-  version "v0.5.7"
-  sha256 "1ff8f69e46c3c6680e89912cd5b57ef7cb0c0225acd3b95a1d90362556af0e2d"
+  url "https://github.com/nacos-group/r-nacos/releases/download/v0.5.9/rnacos-x86_64-apple-darwin.tar.gz"
+  version "v0.5.9"
+  sha256 "20d215565fefadd2369508e50972aed68d1b7f1b1cc6722d338c6187c830e0e4"
   license "Apache-2.0 license"
 
   # depends_on "cmake" => :build
+
+  on_macos do
+    if Hardware::CPU.arm?
+      sha256 "6153293768db8105b65297871636a8e8550d13be76877babe1202efd39cd5789"
+      url "https://github.com/nacos-group/r-nacos/releases/download/v0.5.9/rnacos-aarch64-apple-darwin.tar.gz"
+    end
+  end
+
+  on_linux do
+    sha256 "ba9ef6504b1a4fd3786d1a56d2a175762b6b5546861e9fdc984b58091379bef2"
+    url "https://github.com/nacos-group/r-nacos/releases/download/v0.5.9/rnacos-x86_64-unknown-linux-musl.tar.gz"
+    if Hardware::CPU.arm?
+      sha256 "2186605c6b0b995ebf037f59c3148d250f680bf21352e009aa095a1194da0cdb"
+      url "https://github.com/nacos-group/r-nacos/releases/download/v0.5.9/rnacos-aarch64-unknown-linux-musl.tar.gz"
+    end
+  end
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
